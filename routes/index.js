5ba14b55114e579d45614fb9a1afd8ac8eb9397b
ryangholland <rghcode@gmail.com>
2023-10-23 20:18:38 -0400

    Loop through messages array and add to view template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "I'm Ryan and I like to party.",
+    user: "Ryan",
+    added: new Date()
+  },
+  {
+    text: "Meow.",
+    user: "Isaiah",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
