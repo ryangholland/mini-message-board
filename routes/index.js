7ebbd2cdf077d53be951a589a0ef66f29b5abc8b
ryangholland <rghcode@gmail.com>
2023-10-23 20:40:50 -0400

    Allow user to submit a new message at /new

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,32 @@ const messages = [
   {
     text: "I'm Ryan and I like to party.",
     user: "Ryan",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Meow.",
     user: "Isaiah",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", (req, res, next) => {
+  console.log(req.body);
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect('/')
 });
 
 module.exports = router;
